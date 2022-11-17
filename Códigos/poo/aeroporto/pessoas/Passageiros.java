084023b5cc6c44d6c3f8789f7f47f64b238330aa
Heithor Urquzia <heithoru@gmail.com>
2022-11-17 15:30:17 -0300

    att

--- Códigos/poo/aeroporto/pessoas/Passageiros.java
+++ Códigos/poo/aeroporto/pessoas/Passageiros.java
@@ -1,5 +1,48 @@
 package poo.aeroporto.pessoas;
 
-public class Passageiros {
+import poo.aeroporto.voos.Assento;
+
+public class Passageiros extends Pessoa{
     
+    private Assento seat;
+    private String tipe_service;
+    private boolean embarked;
+
+
+    //Construtor
+    public Passageiros(String name, String cpf, String birth, Assento seat, String tipe_service, boolean embarked) {
+        super(name, cpf, birth);
+        this.seat = seat;
+        this.tipe_service = tipe_service;
+        this.embarked = embarked;
+    }
+
+
+    //Get e Set
+    public Assento getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Assento seat) {
+        this.seat = seat;
+    }
+
+    public String getTipe_service() {
+        return tipe_service;
+    }
+
+    public void setTipe_service(String tipe_service) {
+        this.tipe_service = tipe_service;
+    }
+
+    public boolean isEmbarked() {
+        return embarked;
+    }
+
+    public void setEmbarked(boolean embarked) {
+        this.embarked = embarked;
+    }
+
+    
+
 }

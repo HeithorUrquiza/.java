7a830164bea3b0682e12f1463291ab061b0e20a9
Heithor Urquzia <heithoru@gmail.com>
2022-11-17 15:07:12 -0300

    Add aeroporto

---
+++ Códigos/poo/aeroporto/pessoas/Aeromoca.java
@@ -0,0 +1,5 @@
+package poo.aeroporto.pessoas;
+
+public class Aeromoca{
+    
+}
---
+++ Códigos/poo/aeroporto/pessoas/Pessoa.java
@@ -0,0 +1,40 @@
+package poo.aeroporto.pessoas;
+
+public class Pessoa {
+    
+    private String name;
+    private String cpf;
+    private String birth;
+    
+    //Construtor
+    public Pessoa(String name, String cpf, String birth) {
+        this.name = name;
+        this.cpf = cpf;
+        this.birth = birth;
+    }
+    
+    //Get e set
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public String getBirth() {
+        return birth;
+    }
+    
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+}
